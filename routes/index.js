c3547e2248d9c487d5ffe30886f3b4ef96f0bbe9
jijujosek <jiju.k.jose@vipps.no>
2021-03-02 17:37:35 +0100

    test log

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log("Vannnnnuuuuuuu.....");
   res.render('index', { title: 'Express', message: process.env.MESSAGE || 'this is development', nodeenv: process.env.NODE_ENV});
 });
 
